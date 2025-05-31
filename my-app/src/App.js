d3786f21caa8bbc014b05d61a2b6070493d855cb
Kalu David <kaludavid411@gmail.com>
2025-05-31 14:15:11 +0100

    created react app

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <h1 className='text-center underline'>
+          Hello World
+        </h1>
+    </div>
+  );
+}
+
+export default App;
